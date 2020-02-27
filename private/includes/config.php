a841f248fb0d9978f614c02bb045dfe88003825e
Boyd Hoogkamer <25848@ma-web.nl>
2020-02-27 14:54:11 +0100

    Update config.php
    Connection naar databse gemaakt.

--- private/includes/config.php
+++ private/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = [
-    'db_host' => '',
-    'db_name' => '',
-    'db_user' => 'root',
-    'db_pass' => '',
+    'db_host' => '127.0.0.1',
+    'db_name' => 'c3621thewall',
+    'db_user' => 'c3621root',
+    'db_pass' => 'ipnNueTJG@4L',
     'view_path' => realpath(__DIR__  . '/../views')
 ];
 
