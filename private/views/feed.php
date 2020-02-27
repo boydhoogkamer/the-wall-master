1cf924d76a244cdda17bc738564096a022a713aa
Boyd Hoogkamer <25848@ma-web.nl>
2020-02-27 16:36:14 +0100

    Update feed.php

--- private/views/feed.php
+++ private/views/feed.php
@@ -3,6 +3,9 @@
 
 <?php foreach ($posts as $post):?>
 <h2><?php echo $post['username'] ?></h2>
+<h2><?php echo $post['userimg'] ?></h2>
+<h2><?php echo $post['image'] ?></h2>
+<h2><?php echo $post['likes'] ?></h2>
 <?php endforeach;?>
 
 <?php require __DIR__ . "/../templates/footer.php"; ?>
